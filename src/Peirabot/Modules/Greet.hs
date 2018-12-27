a85c63bb304417624a9f38b0cbe4bdd209c69c13
wkirschbaum <wkirschbaum@gmail.com>
2018-12-28 00:17:56 +0200

    Fix greeting

--- src/Peirabot/Modules/Greet.hs
+++ src/Peirabot/Modules/Greet.hs
@@ -6,13 +6,14 @@ import           Data.Time.LocalTime
 import           Peirabot.Bot
 
 commandHello :: BotInput -> BotAction
-commandHello (BotInput input _ _)
-  | "hi" `isPrefixOf` input = BotResult 2 "Hi :)"
-  | "hello" `isPrefixOf` input = BotResult 2 "Hello :)"
-  | "morning" == input  = BotResult 10 "Hello :)"
-  | "evening" == input  = BotResult 10 "Hello :)"
-  | "afternoon" == input  = BotResult 10 "Hello :)"
+commandHello (BotInput input time rNum)
+  | "hi" `isPrefixOf` input = BotResult 10 greeting
+  | "hello" `isPrefixOf` input = BotResult 10 greeting
+  | "morning" == input  =  BotResult 10 greeting
+  | "evening" == input  =  BotResult 10 greeting
+  | "afternoon" == input  =  BotResult 10 greeting
   | otherwise = BotNoResult
+  where greeting = getGreeting (greetingForDay time) rNum
 
 commandBye :: BotInput -> BotAction
 commandBye (BotInput input _ _)
@@ -27,3 +28,30 @@ greetingForDay (ZonedTime (LocalTime _ (TimeOfDay hour _ _)) zone)
   | hour > 18 = Evening
   | hour > 12 = Afternoon
   | otherwise = Morning
+
+pickFromList :: Int -> [a] -> a
+pickFromList r lst =
+  lst !! i
+  where i = r `mod` (length lst)
+
+getGreeting :: DaySection -> Int -> String
+getGreeting Morning num =
+  pickFromList num [
+    "Good morning!",
+    "Morning buddy.",
+    "Hi.",
+    "Good to see you."
+  ]
+getGreeting Afternoon num =
+  pickFromList num [
+    "Good afternoon!",
+    "Howzit.",
+    "Hey.",
+    "What's up?"
+  ]
+getGreeting Evening num =
+  pickFromList num [
+    "Good evening!",
+    "Hi.",
+    "Hey there."
+  ]
